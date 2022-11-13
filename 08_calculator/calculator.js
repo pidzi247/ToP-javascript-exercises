cac40a2e774e512af7c193bba557e04f31f4c09b
Marek Truska <truskamarek@centrum.sk>
2022-11-13 20:56:20 +0100

    calculator exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  
+	return arr.reduce((acc, value) => acc + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, value) => acc * value);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return Math.pow(n1, n2)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n < 2) {
+    return 1;
+  }
+	return n*factorial(n-1);
 };
 
 // Do not edit below this line
