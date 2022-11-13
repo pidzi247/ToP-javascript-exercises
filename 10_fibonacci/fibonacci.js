0172eb8e135d62712350c4b15cf82088179f2fd7
Marek Truska <truskamarek@centrum.sk>
2022-11-13 21:11:55 +0100

    exercise 10 finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num < 0) {
+    return "OOPS";
+  }
+  let arr = [0,1];
+  for(let i = 0; i <= num; i++) {
+    arr.push(arr[i]+arr[i+1]);
+  }
+  return arr[num];
 };
 
 // Do not edit below this line
