05deff8e0d9276e57f36095e79442df26d060369
Marek Truska <truskamarek@centrum.sk>
2022-11-13 21:39:17 +0100

    exercise 12 finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  
+  const result = arr
+          .sort( function(a, b) { 
+            let nextPerson = b.yearOfDeath - b.yearOfBirth;
+            let previousPerson = a.yearOfDeath - a.yearOfBirth;
+            if(!a.yearOfDeath ) {
+               previousPerson = new Date().getFullYear() - a.yearOfBirth;
+            }
+            if(!b.yearOfDeath) {
+               nextPerson = new Date().getFullYear() - b.yearOfBirth;
+            }
+            return nextPerson - previousPerson;
+          });
 
+  return result[0];
 };
 
 // Do not edit below this line
